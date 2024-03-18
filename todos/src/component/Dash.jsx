9f0233918948e4efede27fdf4a8bf59859c1216e
SurajV2000 <sv107719@gmail.com>
2024-03-18 20:05:51 +0530

    changes done

--- todos/src/component/Dash.jsx
+++ todos/src/component/Dash.jsx
@@ -1,27 +1,17 @@
 import React, { useEffect, useState } from "react";
 import "../css/dash.css";
 function Dash() {
-  let columnname = ["TODO", "INPROGRESS", "REVIEW", "DONE"];
+  const columnname = ["TODO", "INPROGRESS", "REVIEW", "DONE"];
+  const [newTask, setNewTask] = useState("");
+  const [draggedTask, setDraggedTask] = useState(null);
 
   const [tasks, setTasks] = useState([
     { id: 1, category: "todo", title: "Learn React" },
   ]);
 
-  const [draggedTask, setDraggedTask] = useState(null);
-  const [newTask, setNewTask] = useState("");
-
   useEffect(() => {
     handleAddTask("todo");
   }, [newTask]);
-
-  const handleAddTask = (category) => {
-    if (newTask.trim() !== "") {
-      const newId = tasks.length + 1;
-      const newTaskObj = { id: newId, title: newTask, category };
-      setTasks([...tasks, newTaskObj]);
-      setNewTask("");
-    }
-  };
 
   const handleDragStart = (e, task) => {
     setDraggedTask(task);
@@ -42,24 +32,21 @@ function Dash() {
     setDraggedTask(null);
   };
 
+  const handleAddTask = (category) => {
+    if (newTask.trim() !== "") {
+      const newId = tasks.length + 1;
+      const newTaskObj = { id: newId, title: newTask, category };
+      setTasks([...tasks, newTaskObj]);
+      setNewTask("");
+    }
+  };
+
   return (
     <>
       <div className="boards">
         {columnname.map((elem) => {
           return (
-            <div
-              key={elem}
-              style={{
-                display: "flex",
-                width: "100%",
-                flexDirection: "column",
-                alignItems: "left",
-                border: "1px solid blue",
-                margin: "10px",
-                borderRadius: "10px",
-                backgroundColor: "#F0F0F0",
-              }}
-            >
+            <div className="column" key={elem}>
               <div
                 style={{
                   paddingLeft: "10px",
@@ -70,19 +57,7 @@ function Dash() {
               </div>
 
               <div
-                style={{
-                  userSelect: "none",
-                  border: "1px solid red",
-                  padding: "16px",
-                  height: "100%",
-                  margin: "8px",
-                  boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
-                  minHeight: "50px",
-                  borderRadius: "10px",
-                  background: false ? "lightblue" : "#F0F0F0",
-
-                  color: "black",
-                }}
+                className="tasklist"
                 onDragOver={handleDragOver}
                 onDrop={(e) => handleDrop(e, `${elem.toLowerCase()}`)}
               >
@@ -93,18 +68,6 @@ function Dash() {
                       draggable
                       onDragStart={(e) => handleDragStart(e, task)}
                       className="task"
-                      style={{
-                        userSelect: "none",
-                        border: "1px solid blue",
-                        padding: 16,
-                        margin: "0 0 8px 0",
-                        boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
-                        minHeight: "50px",
-                        borderRadius: "10px",
-                        background: false ? "lightblue" : "#F0F0F0",
-
-                        color: "black",
-                      }}
                     >
                       {task.title}
                     </div>
@@ -120,13 +83,6 @@ function Dash() {
                     } else {
                       return;
                     }
-                  }}
-                  style={{
-                    padding: "10px",
-                    color: "black",
-                    width: "40%",
-                    margin: "10px",
-                    marginTop: "-5px",
                   }}
                 >
                   + Add a card
