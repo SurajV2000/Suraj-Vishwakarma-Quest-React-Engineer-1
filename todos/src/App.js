88ea5a737e6838ef350dec6c44fe1158120b592d
SurajV2000 <sv107719@gmail.com>
2024-03-18 19:51:12 +0530

    changes done

--- todos/src/App.js
+++ todos/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Dashboard from "./component/Dashboard";
+import Dash from "./component/Dash";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Dash />
     </div>
   );
 }
